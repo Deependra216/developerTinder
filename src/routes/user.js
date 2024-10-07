831eceabbf12556e8a6beba9304382794e22ddd1
deependra <sdeependra216@gmail.com>
2024-10-07 21:39:33 +0530

    api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const ConnectionRequest = require('../models/connectionRequest')
-const USER_SAFE_DATA = [ "firstName","lastName","gender","age","photoUrl","skills"] 
+const User = require('../models/user');
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
 //find all pending connections requests for loggedIn user
 userRouter.get('/user/requests/received',userAuth,async(req,res)=>{
 try{
@@ -45,6 +47,52 @@ userRouter.get('/user/connections',userAuth,async (req,res)=>{
         res.json({data})
     }catch(err){
         res.status(400).send({message: err.message})
+    }
+})
+
+//user sholud see all others cards except
+//0. his own card
+//1. his connections 
+//2. ignored people
+//3. already sent the connection request to someone
+
+userRouter.get('/feed',userAuth,async(req,res)=>{
+    try{
+        const loggedInUser =req.user;
+        //store pagination number dynamically
+        const page = parseInt(req.query.page) || 1; //it give result like 1 or 2 and if not then assume 1
+        let limit =parseInt(req.query.limit) ||10;
+        limit = limit > 50 ? 50: limit;
+
+        const skip = (page-1)*limit
+        //find all the connection request(send ,received)
+        const connectionRequest = await ConnectionRequest.find({
+            $or:[
+                {fromUserId:loggedInUser._id},
+                {toUserId:loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId")
+       
+        
+        //using new Set() it will just add all values in array but not add duplicate value like add A, B, C, A here Awill not add again
+        const hideUserFromFeed = new Set();
+        connectionRequest.forEach( req =>{
+            hideUserFromFeed.add(req.fromUserId.toString());
+            hideUserFromFeed.add(req.toUserId.toString());
+        })
+       
+        //find all the people whose id not in hideUserFromFeed
+
+        const users = await User.find({
+            $and:[
+                {_id: { $nin:Array.from(hideUserFromFeed)},},
+                {_id: { $ne:loggedInUser._id } }
+            ]
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+        res.json({message:"data of User",data:users,})
+    }
+    catch(err){
+        res.status(400).json({message:err.message})
     }
 })
 
